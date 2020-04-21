ea4055695ee5fa6a249b75f59134387ac98d705a
chrismatteson <cmatteson@hashicorp.com>
2020-04-20 23:02:16 -0700

    Update outputs.tf

--- awsflasktiny/outputs.tf
+++ awsflasktiny/outputs.tf
@@ -1,21 +1,9 @@
-output "project_tag" {
-  value = var.project_tag
+output "eu_instance_availability_zone" {
+  value = aws_instance.eu-web.availability_zone
 }
 
-output "us_instance_public_ip" {
-  value = "http://${aws_instance.us-web.public_ip}:8000"
-}
-
-output "us_instance_vpc_id" {
-  value = aws_vpc.us-vpc.id
-}
-
-output "us_instance_id" {
-  value = aws_instance.us-web.id
-}
-
-output "us_instance_availability_zone" {
-  value = aws_instance.us-web.availability_zone
+output "eu_instance_id" {
+  value = aws_instance.eu-web.id
 }
 
 output "eu_instance_public_ip" {
@@ -26,11 +14,23 @@ output "eu_instance_vpc_id" {
   value = aws_vpc.eu-vpc.id
 }
 
-output "eu_instance_availability_zone" {
-  value = aws_instance.eu-web.availability_zone
+output "project_tag" {
+  value = var.project_tag
 }
 
-
-output "eu_instance_id" {
-  value = aws_instance.eu-web.id
+output "us_instance_availability_zone" {
+  value = aws_instance.us-web.availability_zone
 }
+
+output "us_instance_id" {
+  value = aws_instance.us-web.id
+}
+
+output "us_instance_public_ip" {
+  value = "http://${aws_instance.us-web.public_ip}:8000"
+}
+
+output "us_instance_vpc_id" {
+  value = aws_vpc.us-vpc.id
+}
+
